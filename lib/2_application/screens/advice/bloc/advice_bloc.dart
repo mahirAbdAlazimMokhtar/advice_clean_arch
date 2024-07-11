743b51141fed56a6e5fc6a43ff3d31c20f4e4257
Mahir <mahr.bwb@gmail.com>
2024-07-11 20:38:46 +0300

    --added same failures and dartz to bloc

--- lib/2_application/screens/advice/bloc/advice_bloc.dart
+++ lib/2_application/screens/advice/bloc/advice_bloc.dart
@@ -1,18 +1,36 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../1_domain/failures/failures.dart';
+import '../../../../1_domain/usecases/advice_usecases.dart';
 
 part 'advice_event.dart';
 part 'advice_state.dart';
 
+const serverFailureMessage = 'Server Error, please try again';
+const cachedFailureMessage = 'uups , Cache Error, please try again';
+const generalFailureMessage = 'Unexpected Error, please try again';
+
 class AdviceBloc extends Bloc<AdviceEvent, AdviceState> {
+  final AdviceUsecases adviceUsecases = AdviceUsecases();
   AdviceBloc() : super(AdviceInitial()) {
     on<AdviceRequestedEvent>((event, emit) async {
       emit(AdviceStateLoading());
-      debugPrint('Fake get Advice triggered');
-      await Future.delayed(const Duration(seconds: 2));
-      debugPrint('Get Advice');
-      emit(const AdviceStateLoaded(advice: 'Fake Advice to test Bloc'));
+      final failureOrGetAdviceData = await adviceUsecases.getAdvice();
+      failureOrGetAdviceData.fold(
+          (failure) =>
+              emit(AdviceStateError(message: _mapFailureToMessage(failure))),
+          (advice) => emit(AdviceStateLoaded(advice: advice.advice)));
     });
+  }
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case const (ServerFailure):
+        return serverFailureMessage;
+      case const (CacheFailure):
+        return cachedFailureMessage;
+      default:
+        return generalFailureMessage;
+    }
   }
 }
